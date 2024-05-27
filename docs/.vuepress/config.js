0d3222043fd6d42f8813b0d566eebcf62148b6f8
jikenu <2961819202@qq.com>
2024-05-27 14:56:34 +0800

    pdf

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,8 +20,8 @@ export default defineUserConfig({
         text: 'PDF',
         collapsible: false,
         children: [
-          {text: "Java八股一",link: '/pages/pdf/八股1.md'},
-          {text: "Java八股二",link: '/pages/pdf/八股2.md'}
+          {text: "Java八股一",link: '/pages/pdf/bagu1.md'},
+          // {text: "Java八股二",link: '/pages/pdf/bagu2.md'}
         ]
       },
       {
