b3101b1635b3cea3d51aaac19b3cdcefdce52246
jikenu <2961819202@qq.com>
2024-05-27 15:38:40 +0800

    pdf

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,12 +1,12 @@
-import { defineClientConfig } from "vuepress/client";
-import Audio from './components/Audio.vue';
-// import Video from './components/Video.vue'
-// import PDView from './components/PDView.vue'
+// import { defineClientConfig } from "vuepress/client";
+// import Audio from './components/Audio.vue';
+// // import Video from './components/Video.vue'
+// // import PDView from './components/PDView.vue'
 
-export default defineClientConfig({
-    enhance: ({ app, router, siteData }) => {
-      app.component("Audio", Audio);
-      // app.component("Video",Video);
-      // app.component("PDView",PDView);
-    },
-  });
+// export default defineClientConfig({
+//     enhance: ({ app, router, siteData }) => {
+//       app.component("Audio", Audio);
+//       // app.component("Video",Video);
+//       // app.component("PDView",PDView);
+//     },
+//   });
